e7fa2b46ed700d84c68a2145e0968737425ae03d
christopheryoung <ccyoung2@illinois.edu>
2019-12-08 23:47:38 -0600

    cleanup some code

--- app/src/main/java/com/example/battleship3/FullscreenActivity.java
+++ app/src/main/java/com/example/battleship3/FullscreenActivity.java
@@ -19,10 +19,7 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import java.util.concurrent.TimeUnit;
+
 
 import androidx.gridlayout.widget.GridLayout;
 import java.util.Random;
--- app/src/main/java/com/example/battleship3/SplashScreen.java
+++ app/src/main/java/com/example/battleship3/SplashScreen.java
@@ -2,13 +2,7 @@ package com.example.battleship3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
 import java.util.concurrent.TimeUnit;
 
 public class SplashScreen extends AppCompatActivity {
